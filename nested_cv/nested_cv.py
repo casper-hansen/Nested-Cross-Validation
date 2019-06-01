6178587cfa8099610d1b655d873dbc2026f92d22
casperbh96 <casperbh.96@gmail.com>
2019-06-01 19:58:15 +0200

    Removed redundant import

--- nested_cv/nested_cv.py
+++ nested_cv/nested_cv.py
@@ -1,7 +1,6 @@
 import logging as log
 import pandas as pd
 import numpy as np
-import logging as log
 from matplotlib import pyplot as plt
 from sklearn.model_selection import KFold, ParameterGrid, ParameterSampler
 from sklearn.metrics import mean_squared_error
